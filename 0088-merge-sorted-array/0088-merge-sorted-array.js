c9c5ea019716d2f13ff5cea0aba5c0a36fc547bb
Arjun Pandit <arjunpandit0840@gmail.com>
2023-07-24 19:52:21 +0530

    Time: 66 ms (24.36%), Space: 41.9 MB (71.65%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -1,14 +1,6 @@
-/**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
 var merge = function(nums1, m, nums2, n) {
     for(let i=m;i<m+n;i++)
              nums1[i]=nums2[i-m]
-    nums1.sort()
-    return nums1.sort((a,b)=>a-b)
+    nums1.sort((a,b)=>a-b)
 
 };
